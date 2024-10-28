e93de144a48d4476d5d53d155095f82f99d2214c
Sharan Yalburgi <sharanyalburgi@gmail.com>
2024-10-28 07:27:00 +0000

    Add visualization of probes

--- probe_lens/probes.py
+++ probe_lens/probes.py
@@ -1,22 +1,51 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
 import torch
 import torch.nn as nn
+from sklearn.metrics import accuracy_score, confusion_matrix, fbeta_score
 from tqdm.autonotebook import tqdm
 
 
 class LinearProbe(nn.Module):
-    def __init__(self, input_dim, output_dim=1, device="cpu"):
+    def __init__(self, input_dim, output_dim=1, device="cpu", class_names=None):
         super().__init__()
         self.linear = nn.Linear(input_dim, output_dim, device=device)
+        self.class_names = class_names
 
     def forward(self, x):
         return self.linear(x)
 
+    def visualize_performance(self, dataloader: torch.utils.data.DataLoader, test=False):
+        preds = []
+        gts = []
+        for X, y in dataloader:
+            pred = self(X)
+            gt = y.argmax(dim=1)
+            preds.append(pred.argmax(dim=1))
+            gts.append(gt)
+        preds = torch.cat(preds)
+        gts = torch.cat(gts)
+
+        accuracy = accuracy_score(gts.cpu(), preds.cpu())
+        f2_score = fbeta_score(gts.cpu(), preds.cpu(), beta=2, average='weighted')
+
+        cm = confusion_matrix(gts.cpu(), preds.cpu())
+        plt.figure(figsize=(10, 7))
+        _class_names = self.class_names if self.class_names else [str(i) for i in range(cm.shape[0])]
+        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=_class_names, yticklabels=_class_names)
+        plt.xlabel("Predicted")
+        plt.ylabel("True")
+        plt.title(f"Confusion Matrix (Accuracy: {accuracy:.4f}, F2 Score: {f2_score:.4f})")
+        if not test:
+            plt.show()
+        return plt
+
     def train_probe(
         self,
         dataloader: torch.utils.data.DataLoader,
-        val_dataloader: torch.utils.data.DataLoader,
-        loss_fn,
         optimizer: torch.optim.Optimizer,
+        val_dataloader: torch.utils.data.DataLoader | None = None,
+        loss_fn: nn.Module = nn.BCEWithLogitsLoss(),
         epochs: int = 10,
         verbose: bool = True,
     ):
@@ -30,11 +59,13 @@ class LinearProbe(nn.Module):
                 loss.backward()
                 optimizer.step()
                 loss_sum += loss.item()
+                if val_dataloader is not None:
+                    dataset_names, datasets = ["train", "val"], [dataloader, val_dataloader]
+                else:
+                    dataset_names, datasets = ["train"], [dataloader]
                 if verbose and (epoch + 1) % 10 == 0:
                     losses = {}
-                    for t, dataloader in zip(
-                        ["train", "val"], [dataloader, val_dataloader]
-                    ):
+                    for t, dataloader in zip(dataset_names, datasets):
                         loss_sum = 0
                         correct = 0
                         size = 0
--- tests/test_probes.py
+++ tests/test_probes.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from sklearn.metrics import accuracy_score, fbeta_score
 
 from probe_lens.probes import LinearProbe
 
@@ -40,9 +41,9 @@ def test_linear_probe_training():
     )
     model.train_probe(
         dataloader,
-        dataloader,
-        loss_fn=nn.MSELoss(),
         optimizer=torch.optim.SGD(model.parameters(), lr=0.01),
+        val_dataloader=dataloader,
+        loss_fn=nn.MSELoss(),
         epochs=1000,
         verbose=True,
     )
@@ -50,3 +51,31 @@ def test_linear_probe_training():
     print("Model bias: ", model.linear.bias)
     print("Model loss: ", nn.MSELoss()(model(x), y))
     assert nn.MSELoss()(model(x), y) < 1e-5
+
+
+def test_linear_probe_visualization():
+    input_dim = 10
+    output_dim = 2
+    batch_size = 5
+    x = torch.randn(batch_size, input_dim)
+    A = torch.randn(input_dim, output_dim)
+    B = torch.randn(output_dim)
+    y = torch.sign(x @ A + B)
+
+    model = LinearProbe(input_dim, output_dim)
+    dataloader = torch.utils.data.DataLoader(
+        torch.utils.data.TensorDataset(x, y), batch_size=batch_size, shuffle=True
+    )
+    model.train_probe(
+        dataloader,
+        torch.optim.SGD(model.parameters(), lr=0.01),
+        val_dataloader=dataloader,
+        epochs=1000,
+        verbose=True,
+    )
+    plot = model.visualize_performance(dataloader, test=True)
+    assert plot is not None
+    # assert isinstance(plot, plt.Figure)
+    accuracy = accuracy_score(y.argmax(dim=1).cpu(), model(x).argmax(dim=1).cpu())
+    f2_score = fbeta_score(y.argmax(dim=1).cpu(), model(x).argmax(dim=1).cpu(), beta=2, average='weighted')
+    assert plot.gca().get_title() == f"Confusion Matrix (Accuracy: {accuracy:.4f}, F2 Score: {f2_score:.4f})"
